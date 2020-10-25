0213513dea62981c0c25d7790c2d0c50af3061f0
j-schumann <114239+j-schumann@users.noreply.github.com>
2020-10-25 15:45:24 +0100

    add: netplan for floating ip

--- scripts/setup-master.sh
+++ scripts/setup-master.sh
@@ -2,6 +2,11 @@
 
 parent_path=`dirname "$0"`
 
+echo "Setting the floating IP $PUBLIC_IP as default..."
+cp $parent_path/../server-files/etc/netplan/60-floating-ip.yaml /etc/netplan/
+sed -i "s/PUBLIC_IP/$PUBLIC_IP/g" /etc/netplan/60-floating-ip.yaml
+netplan apply
+
 echo "Setting up security..."
 $parent_path/init-security-master.sh
 
