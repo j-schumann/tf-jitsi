957c693e2fc14679a8330aab9c6039cec9bc18ab
root <114239+j-schumann@users.noreply.github.com>
2020-10-26 11:32:25 +0100

    upd: wait for prosody container

--- scripts/create-prosody-users.sh
+++ scripts/create-prosody-users.sh
@@ -3,11 +3,22 @@
 # required parameters:
 # PROSODY_USERS = username:pwd;username2:pwd2;... 
 
+
+# wait till the container is up
+until [ ! -z $(docker ps -q -f name=meet_prosody) ]
+do
+     sleep 5
+     echo -n "."
+done
+echo ""
+
+# set the field separator
 IFS=';'
 
-#USERSTRING=$(cat /tmp/prosody_users.txt)
-
+# iterate over each user, automatically split at separator
 for ele in $PROSODY_USERS; do
+    # replace occurrence of : with ; - this allows array access (internally split at field separator again)
     USERDATA=(${ele//:/;})
-    docker exec $(docker ps -q -f name=meet_prosody)  prosodyctl --config /config/prosody.cfg.lua register ${USERDATA[0]} meet.jitsi ${USERDATA[1]}
+
+    docker exec $(docker ps -q -f name=meet_prosody) prosodyctl --config /config/prosody.cfg.lua register ${USERDATA[0]} meet.jitsi ${USERDATA[1]}
 done
